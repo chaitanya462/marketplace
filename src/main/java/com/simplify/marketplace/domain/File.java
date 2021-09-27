466c1a3bdfb231e7087e5c26e3f341fa40e598ba
Varshini1101 <ragavarshini11@gmail.com>
2021-09-27 18:17:06 +0530

    resumechanges

--- src/main/java/com/simplify/marketplace/domain/File.java
+++ src/main/java/com/simplify/marketplace/domain/File.java
@@ -1,12 +1,21 @@
 package com.simplify.marketplace.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.simplify.marketplace.domain.enumeration.FileFormat;
 import com.simplify.marketplace.domain.enumeration.FileType;
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * A File.
@@ -52,6 +61,26 @@ public class File implements Serializable {
     @Column(name = "is_profile_pic")
     private Boolean isProfilePic;
 
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Date, format = DateFormat.date)
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Date, format = DateFormat.date)
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+    
     @ManyToOne
     @JsonIgnoreProperties(
         value = { "user", "files", "educations", "certificates", "employments", "portfolios", "refereces", "jobPreferences", "skills" },
--- src/main/java/com/simplify/marketplace/repository/FileRepository.java
+++ src/main/java/com/simplify/marketplace/repository/FileRepository.java
@@ -3,10 +3,13 @@ package com.simplify.marketplace.repository;
 import com.simplify.marketplace.domain.File;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 /**
  * Spring Data SQL repository for the File entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface FileRepository extends JpaRepository<File, Long> {}
+public interface FileRepository extends JpaRepository<File, Long> {
+    List<File> findByWorkerId(Long workerid);
+}
