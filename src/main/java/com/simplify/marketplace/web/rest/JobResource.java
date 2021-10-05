d20c86f9f85a0a6d5cd2cb88c9280c147e468b6d
Kandukuri Ratna Prakarsha <prakarsha2000@gmail.com>
2021-10-05 12:49:23 +0530

    view job details by job id

--- src/main/java/com/simplify/marketplace/web/rest/JobResource.java
+++ src/main/java/com/simplify/marketplace/web/rest/JobResource.java
@@ -55,5 +55,43 @@ public class JobResource {
         JSONObject jobs = (JSONObject) getparser.parse(getresult);  
         return jobs;
     }
+    @GetMapping("/jobs/{id}")
+    public JSONObject  Getjob(@PathVariable String id) throws Exception{
+
+        
+        HttpPost post = new HttpPost("https://qa-services.simplifysandbox.net/authenticate");
+        // add request parameter, form parameters
+        JSONObject json = new JSONObject();
+        json.put("username", "Gauravj@simplifyvms.com"); 
+        json.put("password", "Gaurav@17");
+
+        StringEntity params = new StringEntity(json.toString());
+        post.addHeader("content-type", "application/json");
+        post.setEntity(params);
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse response = httpClient.execute(post);
+        
+        System.out.println("\n\n\n\n---------------------------\n\n\n\n");
+        
+        // System.out.println(EntityUtils.toString(response.getEntity()));
+        String result = EntityUtils.toString(response.getEntity());
+        JSONParser parser = new JSONParser();  
+        JSONObject jsn = (JSONObject) parser.parse(result);  
+        String jwt= jsn.get("token").toString();
+        
+        System.out.println("\n\n\n\n---------------------------\n\n\n\n");
+        
+        HttpGet get = new HttpGet("https://qa-services.simplifysandbox.net/job-manager/programs/99e3e918-3f69-4938-8ccb-46a86b45bc7a/jobs/"+id);
+
+        // add request headers
+        get.addHeader("Authorization", "Bearer " + jwt);
+        HttpResponse getresponse = httpClient.execute(get);
+        String getresult = EntityUtils.toString(getresponse.getEntity());
+        JSONParser getparser = new JSONParser();  
+        JSONObject job = (JSONObject) getparser.parse(getresult);  
+        return job;
+
+    }
 }
 
